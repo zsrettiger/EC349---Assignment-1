893fd06ecbbe049c762c4eba8c0aa6e8af5e515d
zsrettiger <148337309+zsrettiger@users.noreply.github.com>
2023-12-05 07:25:40 +0000

    Update EC349 - a1 code.R

--- EC349 - a1 code.R
+++ EC349 - a1 code.R
@@ -46,10 +46,9 @@ nrow(test)
     rpart_predictionc <- predict(rpart_treec, newdata = test, type="class")
   
     #Calculate accuracy
-    correctly_predicted_rpartc <- sum(rpart_predictionc == test_y)
     # Divide the correctly specified number of stars by the number of stars in the test set 
-    accuracy_rpartc <- correctly_predicted_rpartc/10000
-    sum(accuracy_rpartc)
+    accuracy_rpartc <- sum(rpart_predictionc == test_y) / nrow(test)
+    cat('Accuracy for Classification Tree (rpart):', accuracy_rpartc, '\n')
   
   #Regression Tree
   rpart_treer<-rpart(stars ~ ., data = train, method = 'anova')
@@ -58,17 +57,18 @@ nrow(test)
   
   #Fit on Test Data
     rpart_predictionr <- predict(rpart_treer, newdata = test)
-    rpart_MSEr<- mean((rpart_predictionr - test_y) ^ 2) #Note how it outperforms OLS
-    summary(rpart_MSEr)
+    
+    rpart_MSEr<- mean((rpart_predictionr - test_y) ^ 2) 
+    cat('MSE for Regression Tree (rpart):', rpart_MSEr, '\n')
+    
   
     #Calculate accuracy
     # Convert predicted values to rounded stars (assuming stars are integers)
-    rounded_predictions_rpartr <- round(rpart_predictionr)
     # Count the number of correctly predicted stars
-    correctly_predicted_rpartr <- sum(rounded_predictions_rpartr == test_y)
     # Divide the correctly specified number of stars by the number of stars in the test set 
-    accuracy_rpartr <- correctly_predicted_rpartr/10000
-    sum(accuracy_rpartr)  
+    accuracy_rpartr <- sum(round(rpart_predictionr) == test_y) / nrow(test)
+    cat('Accuracy for Regression Tree (rpart):', accuracy_rpartr, '\n')
+    
 
 #Bagging
 set.seed(123)     
@@ -80,17 +80,17 @@ bag <- bagging(stars ~., data = train, nbagg = 20,
   
   #Fit on Test Data
   bag_predictions <- predict(bag, newdata = test)
-  bag_MSE<- mean((bag_predictions - test_y) ^ 2) #Note how it outperforms OLS
-  summary(bag_MSE)
+  
+  bag_MSE<- mean((bag_predictions - test_y) ^ 2) 
+  cat('MSE for Bagging:', bag_MSE, '\n')
   
   #Calculate accuracy
   # Convert predicted values to rounded stars (assuming stars are integers)
-  rounded_predictions_bag <- round(bag_predictions)
   # Count the number of correctly predicted stars
-  correctly_predicted_bag <- sum(rounded_predictions_bag == test_y)
   # Divide the correctly specified number of stars by the number of stars in the test set 
-  accuracy_bag <- correctly_predicted_bag/10000
-  sum(accuracy_bag)
+  accuracy_bag <- sum(round(bag_predictions) == test_y) / nrow(test)
+  cat('Accuracy for Bagging:', accuracy_bag, '\n')
+  
   
 ## Random Forest
 stars_f <- as.factor(train$stars)
@@ -99,13 +99,14 @@ model_RF<-randomForest(stars_f ~.,data=train, ntree=20)
 model_RF
 pred_RF_test = predict(model_RF, test)
 RF_err <- mean(model_RF[["err.rate"]])
-summary(RF_err)
+cat('Error rate for Random Forest:', RF_err, '\n')
 
+#Calculate accuracy
 # Count the number of correctly predicted stars
-correctly_predicted_rf <- sum(pred_RF_test == test_y)
 # Divide the correctly specified number of stars by the number of stars in the test set 
-accuracy_rf <- correctly_predicted_rf/10000
-sum(accuracy_rf)
+accuracy_rf <- sum(pred_RF_test == test_y) / nrow(test)
+cat('Accuracy for Random Forest:', accuracy_rf, '\n')
+importance(model_RF)
 
 
 ##Linear Regression
@@ -119,16 +120,14 @@ lm_stars_predict<-predict(lm_stars, newdata = test)
 
 #Empirical MSE in TEST data
 lm_stars_test_MSE<-mean((lm_stars_predict-test$stars)^2)
-summary(lm_stars_test_MSE) 
+cat('MSE for OLS:', lm_stars_test_MSE, '\n')
 
 #Calculate accuracy
 # Convert predicted values to rounded stars (assuming stars are integers)
-rounded_predictions_lm <- round(lm_stars_predict)
 # Count the number of correctly predicted stars
-correctly_predicted_lm <- sum(rounded_predictions_lm == test_y)
 # Divide the correctly specified number of stars by the number of stars in the test set 
-accuracy_lm <- correctly_predicted_lm/10000
-sum(accuracy_lm)
+accuracy_lm <- sum(round(lm_stars_predict) == test_y) / nrow(test)
+cat('Accuracy for OLS:', accuracy_lm, '\n')
 
 ##Ridge with Cross-Validation 
 cv.out <- cv.glmnet(as.matrix(train_x), as.matrix(train_y), alpha = 0)
@@ -141,16 +140,14 @@ ridge.mod<-glmnet(train_x, train_y, alpha = 0, lambda = lambda_ridge_cv, thresh 
 #Fit on Test Data
 ridge.pred <- predict(ridge.mod, s = lambda_ridge_cv, newx = as.matrix(test_x))
 ridge_MSE<- mean((ridge.pred - test_y) ^ 2) #Outperforms OLS
-summary(ridge_MSE)
+cat('MSE for RIDGE:', ridge_MSE, '\n')
 
 #Calculate accuracy
 # Convert predicted values to rounded stars (assuming stars are integers)
-rounded_predictions_ridge <- round(ridge.pred)
 # Count the number of correctly predicted stars
-correctly_predicted_ridge <- sum(rounded_predictions_ridge == test_y)
 # Divide the correctly specified number of stars by the number of stars in the test set 
-accuracy_ridge <- correctly_predicted_ridge/10000
-sum(accuracy_ridge)
+accuracy_ridge <- sum(round(ridge.pred) == test_y) / nrow(test)
+cat('Accuracy for RIDGE:', accuracy_ridge, '\n')
 
 ##LASSO with Cross-Validation 
 cv.out <- cv.glmnet(as.matrix(train_x), as.matrix(train_y), alpha = 1, nfolds = 3)
@@ -159,18 +156,16 @@ lambda_LASSO_cv <- cv.out$lambda.min #cross-validation is the lambda minimising 
 
 #Estimate LASSO with lambda chosen by Cross validation
 LASSO.mod<-glmnet(train_x, train_y, alpha = 1, lambda = lambda_LASSO_cv, thresh = 1e-12)
-coef(LASSO.mod) #note that some parameter estimates are set to 0 --> Model selection!
+coef(LASSO.mod) 
 
 #Fit on Test Data
 LASSO.pred <- predict(LASSO.mod, s = lambda_LASSO_cv, newx = as.matrix(test_x))
-LASSO_MSE<- mean((LASSO.pred - test_y) ^ 2) #Note how it outperforms OLS
-summary(LASSO_MSE)
+LASSO_MSE <- mean((LASSO.pred - test_y) ^ 2) #Note how it outperforms OLS
+cat('MSE for LASSO:', LASSO_MSE, '\n')
 
 #Calculate accuracy
 # Convert predicted values to rounded stars (assuming stars are integers)
-rounded_predictions_lasso <- round(LASSO.pred)
 # Count the number of correctly predicted stars
-correctly_predicted_lasso <- sum(rounded_predictions_lasso == test_y)
 # Divide the correctly specified number of stars by the number of stars in the test set 
-accuracy_lasso <- correctly_predicted_lasso/10000
-sum(accuracy_lasso)
+accuracy_lasso <- sum(round(LASSO.pred) == test_y) / nrow(test)
+cat('Accuracy for LASSO:', accuracy_lasso, '\n')
